410bc020b165b4ba000350e68ce277d7e90d5a02
aimee-liang <aimeeeliang@gmail.com>
2020-11-07 14:08:34 -0500

    Added cuisines table, association, and model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
     has_many :locations
     has_many :reviews
     has_many :users, through: :reservations
+    has_many :cuisines
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_010415) do
+ActiveRecord::Schema.define(version: 2020_11_07_185737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cuisines", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "cuisine_id"
+    t.string "cuisine_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "address"
