88a018dc3914e3b204e49b75bf80498af0c602b8
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-07 23:43:04 -0500

    changed restaurant migration, revised seeds

--- db/migrate/20201106192806_create_restaurants.rb
+++ db/migrate/20201106192806_create_restaurants.rb
@@ -2,8 +2,11 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
       t.string :name
+      t.integer :zomato_id
       t.string :cuisines
       t.string :address
+      t.string :zip
+      t.string :photos_url
       t.timestamps
     end
   end
