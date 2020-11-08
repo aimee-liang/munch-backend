726d13c1e51972921448ed22e1eda3b3b29d1728
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-08 00:11:59 -0500

    turned off auth for testing

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RestaurantsController < ApplicationController
-    before_action :authorized
+    #before_action :authorized
     def index
         restaurants =  Restaurant.all
         render json: restaurants
